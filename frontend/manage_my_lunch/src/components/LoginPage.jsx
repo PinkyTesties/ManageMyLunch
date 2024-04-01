4222a478b8d58528b208d811b75f3bde05c85c14
vidhusan12 <vidhusan709@gmail.com>
2024-04-02 03:30:52 +1300

    Update Login page logic

--- frontend/manage_my_lunch/src/components/LoginPage.jsx
+++ frontend/manage_my_lunch/src/components/LoginPage.jsx
@@ -4,37 +4,62 @@ import axios from 'axios';
 import logo from './componentAssets/logov1.png';
 import { ToastContainer, toast } from 'react-toastify';
 
-
 axios.defaults.withCredentials = true;
 
-const LoginPage = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+const Login = () => {
   const navigate = useNavigate();
-  const [error, setError] = useState("");
+  const [inputValue, setInputValue] = useState({
+    email: "",
+    password: "",
+  });
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    axios.post("http://localhost:8082/api/login", { email, password })
-      .then(res => {
-        console.log(res);
-        navigate('/dashboard');
-      })
-      .catch(err => {
-        console.log(err);
-        setError("Invalid email or password");
+  const { email, password } = inputValue;
 
-      });
-  }
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
+    setInputValue({ ...inputValue, [name]: value });
+  };
+
+  const handleSuccess = (msg) => {
+    toast.success(msg, {
+      position: "bottom-left",
+    });
+  };
+
+  const handleErrors = (err) => {
+    toast.error(err, {
+      position: "bottom-left",
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios.post("http://localhost:8082/login", inputValue, { withCredentials: true });
+      console.log(data);
+      const { success, message } = data;
+      if (success) {
+        handleSuccess(message);
+        setTimeout(() => {
+          navigate("/dashboard");
+        }, 1000);
+      } else {
+        handleErrors(message);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+    setInputValue({
+      email: "",
+      password: "",
+    });
+  };
 
   return (
     <div className="container mt-5">
-      {error && <div className="alert alert-danger">{error}</div>}
       <img src={logo} alt='Logo' height={100} />
-
-      <h2>Login</h2>
+      <h2>Login Account</h2>
       <p>***THIS PAGE STILL REQUIRES CSS. DO NOT SUBMIT AS IS***</p>
-
       <form onSubmit={handleSubmit}>
         <div className="mb-3">
           <label htmlFor="email" className="form-label">
@@ -45,7 +70,8 @@ const LoginPage = () => {
             className="form-control"
             id="email"
             value={email}
-            onChange={(e) => setEmail(e.target.value)}
+            placeholder='Enter your email address'
+            onChange={handleOnChange}
             required
           />
         </div>
@@ -58,7 +84,8 @@ const LoginPage = () => {
             className="form-control"
             id="password"
             value={password}
-            onChange={(e) => setPassword(e.target.value)}
+            placeholder='Enter your password'
+            onChange={handleOnChange}
             required
           />
         </div>
@@ -68,13 +95,14 @@ const LoginPage = () => {
         <Link to="/sign-up" className="btn btn-link">
           Sign Up
         </Link>
-      <br></br>
+        <br></br>
         <Link to="/UpdatePassword" className="btn btn-link">
           I forgot my password
         </Link>
+        <ToastContainer />
       </form>
     </div>
   );
 };
 
-export default LoginPage;
+export default Login;
