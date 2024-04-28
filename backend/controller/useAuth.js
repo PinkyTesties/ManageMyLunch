8d43e6d48f048273e4e1cf2fcd576b81cc04b135
Tyler Costa <tyler@downhead.net>
2024-04-28 21:23:59 +1200

    Update useAuth.js

--- backend/controller/useAuth.js
+++ backend/controller/useAuth.js
@@ -1,12 +1,13 @@
 // useAuth.js
 import { useState } from 'react';
 import axios from 'axios';
+import { backendURL } from './../urls'; // import backendURL from urls.js
 
 export function useAuth() {
   const [token, setToken] = useState(null);
 
   const login = async (email, password) => {
-    const response = await axios.post('/api/login', { email, password });
+    const response = await axios.post(`${backendURL}/api/login`, { email, password });
     setToken(response.data.token);
   };
 
