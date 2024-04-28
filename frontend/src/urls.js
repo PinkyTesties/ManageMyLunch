1e6fb9c364ef4142bbfe2fc106d6d590b8e9a5d3
Tyler Costa <tyler@downhead.net>
2024-04-28 19:25:00 +1200

    Again

--- frontend/src/urls.js
+++ frontend/src/urls.js
@@ -1,5 +1,5 @@
 //This is the backend URL for production
-//export const backendURL = `https://managemylunch.onrender.com`;
+export const backendURL = `https://managemylunch.onrender.com`;
 
 //This is the backend URL for development
-export const backendURL = `http://localhost:8082`;
+//export const backendURL = `http://localhost:8082`;
